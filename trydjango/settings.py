1b4e9674a59f7e38732803ba62a4b4cc2e20ec76
wellersy <41797021+wellersy@users.noreply.github.com>
2022-06-25 16:28:14 +0200

    Update settings.py
    added print (DEBUG)

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
+print(DEBUG)
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
